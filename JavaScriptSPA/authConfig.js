0901b451e7cb76704d82c8b850f3c3a4669a46e5
Longfei Sun <jimmy@tech1108.com>
2020-12-27 14:59:23 +0000

    Updated authConfig.js

--- JavaScriptSPA/authConfig.js
+++ JavaScriptSPA/authConfig.js
@@ -11,8 +11,8 @@ const msalConfig = {
   auth: {
     clientId: "32a57c0a-2f0a-4942-8093-bc8b906f025f",
     authority: "https://login.microsoftonline.com/common",
-    redirectUri: "https://localhost",
-    //redirectUri: "http://sts.azurehybrid.tk"
+    //redirectUri: "https://localhost",
+    redirectUri: "https://jimmytestspa.azurewebsites.net"
   },
     system: {
       logger: new Msal.Logger(
